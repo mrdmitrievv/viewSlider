464db0424e3f3b6b69a67229291ebeb8392efefa
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-02-26 22:33:32 +0300

    Initial Commit

---
+++ viewSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewSlider
+//
+//  Created by Артём Дмитриев on 26.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
