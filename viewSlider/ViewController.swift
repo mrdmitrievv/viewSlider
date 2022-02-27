2df95fcac7d6237219b8f6544f1e66f0b764218e
mrdmitrievv <mrdmitrievv@yandex.ru>
2022-02-27 13:55:57 +0300

    Fixed bags and finished the app

--- viewSlider/ViewController.swift
+++ viewSlider/ViewController.swift
@@ -1,16 +1,8 @@
-//
-//  ViewController.swift
-//  viewSlider
-//
-//  Created by Артём Дмитриев on 26.02.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
         
     // MARK: Outlets for value labels and main view
-    
     @IBOutlet var mainView: UIView!
     @IBOutlet var redColorValue: UILabel!
     @IBOutlet var greenColorValue: UILabel!
@@ -25,61 +17,42 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        redSlider.value = 0
-        redSlider.minimumValue = 0
-        redSlider.maximumValue = 255
+        redSlider.thumbTintColor = .red
+        greenSlider.thumbTintColor = .green
+        alphaSlider.thumbTintColor = .black
         
-        greenSlider.value = 0
-        greenSlider.minimumValue = 0
-        greenSlider.maximumValue = 255
-        
-        blueSlider.value = 0
-        blueSlider.minimumValue = 0
-        blueSlider.maximumValue = 255
-        
-        alphaSlider.value = 1
-        alphaSlider.minimumValue = 0
-        alphaSlider.maximumValue = 1
-        
-        redColorValue.text = String(Int(redSlider.value))
-        greenColorValue.text = String(Int(greenSlider.value))
-        blueColorValue.text = String(Int(blueSlider.value))
-        alphaValue.text = String(alphaSlider.value)
+        redColorValue.text = "n/a"
+        greenColorValue.text = "n/a"
+        blueColorValue.text = "n/a"
+        alphaValue.text = "n/a"
         
     }
-        
     
+    // MARK: Function that changes mainView background color
+    func changeViewColor() {
+        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value) / 255, green: CGFloat(greenSlider.value) / 255, blue: CGFloat(blueSlider.value) / 255, alpha: CGFloat(alphaSlider.value))
+    }
+        
+    // MARK: Actions on sliders that change label texts and mainView color
     @IBAction func redSliderAction() {
         redColorValue.text = String(Int(redSlider.value))
-        redSlider.value = redSlider.value
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        changeViewColor()
     }
     
     @IBAction func greenSliderAction() {
         greenColorValue.text = String(Int(greenSlider.value))
-        greenSlider.value = greenSlider.value
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        changeViewColor()
     }
-    
-    
     
     @IBAction func blueSliderAction() {
         blueColorValue.text = String(Int(blueSlider.value))
-        blueSlider.value = blueSlider.value
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        changeViewColor()
     }
     
     @IBAction func alphaSliderAction() {
         alphaValue.text = String(alphaSlider.value)
-        alphaSlider.value = alphaSlider.value
-        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        changeViewColor()
     }
-    
-    
-//        mainView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
-
-    
 }
 
